106cb50c6d59dc10c679f098b5adfddfb8f6b03c
Jorge Modesto <7728049+jorgemdnt@users.noreply.github.com>
2022-12-11 16:03:50 +0100

    Day 11 - part 2

--- day11/solution.rb
+++ day11/solution.rb
@@ -6,42 +6,6 @@ TEST = /Test: divisible by (?<by>\d+)/.freeze
 IF_TRUE = /If true: throw to monkey (?<monkey>\d+)/.freeze
 IF_FALSE = /If false: throw to monkey (?<monkey>\d+)/.freeze
 
-class Monkey
-  attr_accessor :items
-  attr_reader :inspections
-
-  def initialize(items:, operation:, test:)
-    @items = items
-    @operation = operation
-    @test = test
-    @inspections = 0
-  end
-
-  def turn(monkeys)
-    while (item = @items.shift)
-      new = inspect(item)
-      reduced = reduce_stress(new)
-      monkeys[test!(reduced)].items << reduced
-    end
-  end
-
-  private
-
-  def inspect(item)
-    @inspections += 1
-    operand = @operation[:operand] == 'old' ? item : @operation[:operand].to_i
-    [item, operand].inject(@operation[:operator])
-  end
-
-  def reduce_stress(item)
-    (item / 3.0).floor
-  end
-
-  def test!(item)
-    (item % @test[:by]).zero? ? @test[:when_true] : @test[:when_false]
-  end
-end
-
 def parse_monkey(raw_monkey)
   Monkey.new(
     items: raw_monkey.match(ITEMS)[:items].split(', ').map(&:to_i),
@@ -57,6 +21,42 @@ def parse_monkey(raw_monkey)
   )
 end
 
+class Monkey
+  attr_accessor :items
+  attr_reader :inspections, :test
+
+  def initialize(items:, operation:, test:)
+    @items = items
+    @operation = operation
+    @test = test
+    @inspections = 0
+  end
+
+  def turn(monkeys, lcm = nil)
+    while (item = @items.shift)
+      worry_level = inspect(item)
+      reduced = reduce_stress(worry_level, lcm)
+      monkeys[next_monkey(reduced)].items << reduced
+    end
+  end
+
+  private
+
+  def inspect(item)
+    @inspections += 1
+    operand = @operation[:operand] == 'old' ? item : @operation[:operand].to_i
+    [item, operand].inject(@operation[:operator])
+  end
+
+  def reduce_stress(item, lcm)
+    lcm ? item % lcm : item / 3
+  end
+
+  def next_monkey(item)
+    (item % @test[:by]).zero? ? @test[:when_true] : @test[:when_false]
+  end
+end
+
 def monkey_business(input)
   monkeys = input.map { |m| parse_monkey(m) }
   20.times do
@@ -67,6 +67,18 @@ def monkey_business(input)
   monkeys.map(&:inspections).sort.last(2).inject(:*)
 end
 
+def monkey_business_v2(input)
+  monkeys = input.map { |m| parse_monkey(m) }
+  lcm = monkeys.map { |m| m.test[:by] }.inject(&:lcm)
+  10_000.times do
+    monkeys.each do |monkey|
+      monkey.turn(monkeys, lcm)
+    end
+  end
+  monkeys.map(&:inspections).sort.last(2).inject(:*)
+end
+
 input = File.read(ARGV.last).split("\n\n")
 
 puts monkey_business(input)
+puts monkey_business_v2(input)
